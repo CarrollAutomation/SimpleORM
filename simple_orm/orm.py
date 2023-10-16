b733266c5b6cbfc02d7715e226175d50a0ea6f59
= <=>
2023-10-16 09:35:42 -0400

    Create initialization for ORM

---
+++ simple_orm/orm.py
@@ -0,0 +1,21 @@
+from simple_orm.models import DBGetQuery, DBObject
+
+DATA_BASE_FILES = {}
+
+
+def initialize_orm(db_path, classes=None):
+    db_path = "C:\\Users\\Cactus\\Documents\\test.db"
+    global DATA_BASE_FILES
+    filename = db_path.split('\\')[-1]
+    db_name = filename.split('.')[0]
+    path = db_path
+    DATA_BASE_FILES[db_name] = path
+
+    if classes is None:
+        classes = DBObject.__subclasses__()
+
+    for _class in classes:
+        _class._file_path = DATA_BASE_FILES[db_name]
+        _class.get = DBGetQuery(_class, DATA_BASE_FILES[db_name])
+        _class._class = _class
+
